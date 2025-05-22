1d9bebef8904c27907496f82b97d077c0fb107e6
Simon <smonek33@gmail.com>
2025-05-22 19:45:09 +0200

    Working version with lower weights

--- Testing_scripts/sentiment_model_tester.py
+++ Testing_scripts/sentiment_model_tester.py
@@ -3,18 +3,23 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
 
-model = load_model('../Trained_models/sentiment_please_dont_brick_my_pc_model_the_second_one.keras')
+model = load_model('../Trained_models/sentiment_model.keras')
 
 texts = ["The author of this book Michael Lane must have been taught how to spell by his son. this stupid son of a bitch mispelled every word in the book. I can understand why he is so dumb mostly because he is crippled, but the fact the books sounds like it was made by the power rangers makes me think this guy is the next michael jackson. in all if you buy this book you are aiding terrorism, and if you read it you are just making yourself that much dumber. SO PLEASE DON'T BUY AND READ THIS BOOK!!!!!!!!!!"]
 
 tokenizer = Tokenizer(num_words=1000, oov_token="<DOV>")
 
-tokenizer.fit_on_texts(texts)
-sample_text = ["This was a great product!", "Horrible and slow service.", "It was okay I guess."]
-sample_seq = tokenizer.texts_to_sequences(sample_text)
-sample_pad = pad_sequences(sample_seq, maxlen=100, padding='post')
-predictions = model.predict(sample_pad)
-predicted_labels = np.argmax(predictions, axis=1) - 1
-print(predicted_labels)
+def predict_review(text):
+    tokenizer.fit_on_texts(texts)
+    seq = tokenizer.texts_to_sequences([text])
+    padded_seq = pad_sequences(seq, maxlen=150, padding='post')
+    pred = model.predict(padded_seq)
+    print("Tokenized sequence:", seq)
+    print("Padded input:", padded_seq)
+    print("Prediction (probabilities):", pred)
+    class_idx = np.argmax(pred)
+    return class_idx + 1
+
+print(predict_review(texts[0]))
 
 
--- Training_script/sentiment_model_trainer.py
+++ Training_script/sentiment_model_trainer.py
@@ -8,7 +8,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional, BatchNormalization
 
 
-df = pd.read_csv('../Data/train.csv', header=None, names=['label','title','review'])
+df = pd.read_csv('../converted_reviews.csv', header=None, names=['label','title','review'], nrows=50000)
 
 texts = df['review'].values
 labels = df['label'].values
@@ -18,7 +18,7 @@ labels = labels - 1
 num_classes = len(np.unique(labels))
 
 labels_one_hot = to_categorical(labels, num_classes=num_classes)
- 
+
 tokenizer = Tokenizer(num_words=50000, oov_token="<DOV>")
 
 tokenizer.fit_on_texts(texts)
@@ -42,14 +42,17 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train, Y_train, epochs=5, validation_data=(X_val, Y_val))
+model.fit(X_train, Y_train, epochs=1, validation_data=(X_val, Y_val))
 
 model.save("../Trained_models/sentiment_model.keras")
 
 def predict_review(text):
     seq = tokenizer.texts_to_sequences([text])
-    padded_seq = pad_sequences(seq, maxlen=padded.shape[1], padding='post')
+    padded_seq = pad_sequences(seq, maxlen=150)
     pred = model.predict(padded_seq)
+    print("Tokenized sequence:", seq)
+    print("Padded input:", padded_seq)
+    print("Prediction (probabilities):", pred)
     class_idx = np.argmax(pred)
     return class_idx + 1
 
@@ -57,6 +60,6 @@ def predict_review(text):
 print(predict_review("ive got a lamp in the corner of my room behind my desk thats a complete pain in the arse to turn on and off. ive been using this with the lamp for a month now and it works perfectly. added a little velcro and now i have a light switch where ever i want. under my desk, shelf, etc."))
 print(predict_review("Very disappointed in this product. It worked perfectly for exactly three days and could not be resuscitated. It was very inexpensive so I did not want to pay half again the price to ship it back for an exchange, so the company would do nothing when they sent me an inquiry as to product satisfaction."))
 print(predict_review("This is the all time best book. She mentoins in the book how anyone can be a vampire, who knows. Well anyway I like the idea of having one soulmate and one crazed werewolf crush. P.S I think that it is a good twist on the story about how anone can be part of the night world."))
-print(predict_review("Can't believe I have never reviewed this book before. First read this back when I was 13. Like a lot of girls wanting the mysterious boyfriend and believed in soul-mates which drug me into these books. Rereading it over time years later still stands the test of time for me. Ash is that bad boy you want and your parents want you to stay away from even though he seems like a jerk there is just something about him that make you wanna root for him."))
+print(predict_review("I suppose if you were going to sit in the same room and have line-of-sight with the device, lamp, etc. plugged into this control, it might work. Maybe. But I had it completely within the stated range and one basic house wall separating me and the unit, and it was completely unreliable.I'd recommend spending a touch more money and getting a higher quality product."))
 
-print("Correct review : 2 1 2 2")
+print("Correct review : 2 1 2 1")
