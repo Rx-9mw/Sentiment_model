2b420dd8ede74ba01aaf0f2533039c430568b4a2
Simon <smonek33@gmail.com>
2025-05-23 04:18:15 +0200

    A lot of changes dealing with the val_acc

--- Testing_scripts/sentiment_model_tester.py
+++ Testing_scripts/sentiment_model_tester.py
@@ -1,25 +1,72 @@
 from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
 import numpy as np
+import pandas as pd
+import pickle
 
-model = load_model('../Trained_models/sentiment_model.keras')
+max_length = 150
+num_of_classes = 2
 
-texts = ["The author of this book Michael Lane must have been taught how to spell by his son. this stupid son of a bitch mispelled every word in the book. I can understand why he is so dumb mostly because he is crippled, but the fact the books sounds like it was made by the power rangers makes me think this guy is the next michael jackson. in all if you buy this book you are aiding terrorism, and if you read it you are just making yourself that much dumber. SO PLEASE DON'T BUY AND READ THIS BOOK!!!!!!!!!!"]
+model = load_model('../Trained_models/Models/sentiment_model_dropout_twice.keras')
 
-tokenizer = Tokenizer(num_words=1000, oov_token="<DOV>")
+with open('../Trained_models/Dictionaries/tokenizer.pkl', 'rb') as f:
+    tokenizer = pickle.load(f)
 
-def predict_review(text):
-    tokenizer.fit_on_texts(texts)
-    seq = tokenizer.texts_to_sequences([text])
-    padded_seq = pad_sequences(seq, maxlen=150, padding='post')
-    pred = model.predict(padded_seq)
-    print("Tokenized sequence:", seq)
-    print("Padded input:", padded_seq)
-    print("Prediction (probabilities):", pred)
-    class_idx = np.argmax(pred)
-    return class_idx + 1
+test_df = pd.read_csv("../Data/converted_reviews.csv", header=None, names=["label", "title", "review"])
 
-print(predict_review(texts[0]))
+texts = test_df["review"].values
+labels = test_df["label"].values
+labels = labels - 1
+
+sequences = tokenizer.texts_to_sequences(texts)
+
+padded = pad_sequences(sequences, maxlen=max_length)
+
+labels_one_hot = to_categorical(labels, num_classes=num_of_classes)
+
+loss, accuracy = model.evaluate(padded, labels_one_hot)
+
+print("\n-----------------------------------------")
+print(loss)
+print(accuracy)
+print("-----------------------------------------\n")
+
+preds = model.predict(padded)
+pred_classes = np.argmax(preds, axis=1)
+pred_classes += 1
+
+print("\n-----------------------------------------")
+print("Sample predictions with reviews:")
+for i in range(10):
+    print(f"Review {i+1}:")
+    print(f"Text      : {texts[i]}")
+    print(f"Predicted : {pred_classes[i]}")
+    print(f"Actual    : {labels[i] + 1}")
+    print("-----------------------------------------\n")
+
+
+
+# def predict_review(text):
+#     seq = tokenizer.texts_to_sequences([text])
+#     padded_seq = pad_sequences(seq, maxlen=max_length)
+#     pred = model.predict(padded_seq)
+#     print("Prediction (probabilities):", pred)
+#     class_idx = np.argmax(pred)
+#     print("Picked prediction:", class_idx)
+#     return class_idx + 1
+
+
+
+# print("-----------------------------------------")
+# print("Correct review : 2 | Predicted review : ", predict_review("ive got a lamp in the corner of my room behind my desk thats a complete pain in the arse to turn on and off. ive been using this with the lamp for a month now and it works perfectly. added a little velcro and now i have a light switch where ever i want. under my desk, shelf, etc."))
+# print("-----------------------------------------")
+# print("Correct review : 1 | Predicted review : ", predict_review("With all due respect to ambient music enthusiasts, I was really disappointed that there was no guitar work whatsoever on this album. Hillage fans of L and Fish Rising be forewarned.Steve Hillage was a pretty darn good guitarist. Maybe L was his showcase with members of Todd Rundgren's Utopia backing him up.Noting that other reviewers have rated this highly, I will give it another listen. However, I am dissapointed in the direction Steve has taken his music."))
+# print("-----------------------------------------")
+# print("Correct review : 1 | Predicted review : ", predict_review("It worked about 40% of the time when I was standing less than two feet from the outlet. When I moved eight to ten feet away it worked about 5% of the. I would not recommend it."))
+# print("-----------------------------------------")
+# print("Correct review : 2 | Predicted review : ", predict_review("I liked the film it has what every horror likes in this type of movie the suspense,mystery,good and bad times,plus a shocking ending that makes you think that there is more to story, and plot was very good Robert."))
+# print("-----------------------------------------")
+
 
 
