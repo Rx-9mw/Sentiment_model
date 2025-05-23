703f1e7caef170d93a675abd935ed3e038c669fb
Simon <smonek33@gmail.com>
2025-05-23 04:18:15 +0200

    A lot of changes dealing with the val_acc

--- Training_script/sentiment_model_trainer.py
+++ Training_script/sentiment_model_trainer.py
@@ -1,14 +1,21 @@
 import pandas as pd
 import numpy as np
+import pickle
+from keras.layers import Bidirectional
+from tensorflow.keras import regularizers
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Bidirectional, BatchNormalization
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from tensorflow.keras.callbacks import EarlyStopping
 
+words = 150000
+max_length = 150
+epochs = 5
 
-df = pd.read_csv('../Data/train.csv', header=None, names=['label','title','review'], nrows=50000)
+df = pd.read_csv('../Data/train.csv', header=None, names=['label','title','review'])
 
 texts = df['review'].values
 labels = df['label'].values
@@ -19,20 +26,44 @@ num_classes = len(np.unique(labels))
 
 labels_one_hot = to_categorical(labels, num_classes=num_classes)
 
-tokenizer = Tokenizer(num_words=50000, oov_token="<DOV>")
+tokenizer = Tokenizer(num_words=words, oov_token="<DOV>")
 
 tokenizer.fit_on_texts(texts)
 
+with open('../Trained_models/Dictionaries/tokenizer.pkl', 'wb') as f:
+    pickle.dump(tokenizer, f)
+
 sequences = tokenizer.texts_to_sequences(texts)
 
-padded = pad_sequences(sequences, maxlen=150)
+padded = pad_sequences(sequences, maxlen=max_length)
 
-X_train, X_val, Y_train, Y_val = train_test_split(padded, labels_one_hot, test_size=0.2, random_state=42)
+# X_train, X_val, Y_train, Y_val = train_test_split(padded, labels_one_hot, test_size=0.2, random_state=42)
+
+X_train = padded
+Y_train = labels_one_hot
+
+test_df = pd.read_csv("../Data/test.csv", header=None, names=["label", "title", "review"])
+test_texts = test_df["review"].values
+test_labels = test_df["label"].values
+test_labels = test_labels - 1
+test_labels_one_hot = to_categorical(test_labels, num_classes=num_classes)
+
+test_sequences = tokenizer.texts_to_sequences(test_texts)
+X_val = pad_sequences(test_sequences, maxlen=max_length)
+Y_val = test_labels_one_hot
+
+early_stopping = EarlyStopping(
+    monitor='val_loss',
+    patience=2,
+    restore_best_weights=True
+)
 
 model = Sequential([
-    Embedding(input_dim=50000, output_dim=32),
-    LSTM(64, return_sequences=False),
-    Dense(64, activation='relu'),
+    Embedding(input_dim=50000, output_dim=16),
+    Bidirectional(LSTM(64, return_sequences=False)),
+    Dropout(0.3),
+    Dense(16, activation='relu', kernel_regularizer=regularizers.l2(0.001)),
+    Dropout(0.3),
     Dense(2, activation='softmax')
 ])
 
@@ -42,24 +73,6 @@ model.summary()
 
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-model.fit(X_train, Y_train, epochs=1, validation_data=(X_val, Y_val))
+model.fit(X_train, Y_train, epochs=epochs, validation_data=(X_val, Y_val), batch_size=64, callbacks=[early_stopping])
 
-model.save("../Trained_models/sentiment_model.keras")
-
-def predict_review(text):
-    seq = tokenizer.texts_to_sequences([text])
-    padded_seq = pad_sequences(seq, maxlen=150)
-    pred = model.predict(padded_seq)
-    print("Tokenized sequence:", seq)
-    print("Padded input:", padded_seq)
-    print("Prediction (probabilities):", pred)
-    class_idx = np.argmax(pred)
-    return class_idx + 1
-
-
-print(predict_review("ive got a lamp in the corner of my room behind my desk thats a complete pain in the arse to turn on and off. ive been using this with the lamp for a month now and it works perfectly. added a little velcro and now i have a light switch where ever i want. under my desk, shelf, etc."))
-print(predict_review("Very disappointed in this product. It worked perfectly for exactly three days and could not be resuscitated. It was very inexpensive so I did not want to pay half again the price to ship it back for an exchange, so the company would do nothing when they sent me an inquiry as to product satisfaction."))
-print(predict_review("This is the all time best book. She mentoins in the book how anyone can be a vampire, who knows. Well anyway I like the idea of having one soulmate and one crazed werewolf crush. P.S I think that it is a good twist on the story about how anone can be part of the night world."))
-print(predict_review("I suppose if you were going to sit in the same room and have line-of-sight with the device, lamp, etc. plugged into this control, it might work. Maybe. But I had it completely within the stated range and one basic house wall separating me and the unit, and it was completely unreliable.I'd recommend spending a touch more money and getting a higher quality product."))
-
-print("Correct review : 2 1 2 1")
+model.save("../Trained_models/Models/sentiment_model_dropout_twice.keras")
