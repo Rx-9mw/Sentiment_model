42a9aae9339b335688116eac11ec83cb5824cf24
Simon <smonek33@gmail.com>
2025-06-22 16:09:38 +0200

    made it more readable

---
+++ Training_script/data/data_processing.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import pickle
+from nltk.corpus import stopwords
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.utils import to_categorical
+
+# #nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+
+def remove_stopwords(text):
+    tokens = text.split()
+    filtered = [w for w in tokens if w.lower() not in stop_words]
+    return " ".join(filtered)
+
+
+def load_and_prepare_data(nrows_val, words, max_length):
+    
+    df = pd.read_csv('../Data/train.csv' , header=None, names=['label','title','review'], nrows=nrows_val)
+    df['review_clean'] = df['review'].astype(str).apply(remove_stopwords)
+    texts = df['review_clean'].values
+    labels = df['label'].values
+    labels = labels - 1
+    num_classes = len(np.unique(labels))
+    labels_one_hot = to_categorical(labels, num_classes=num_classes)
+
+    tokenizer = Tokenizer(num_words=words, oov_token="<DOV>")
+    tokenizer.fit_on_texts(texts)
+
+    with open('../Trained_models/Dictionaries/tokenizer.pkl', 'wb') as f:
+        pickle.dump(tokenizer, f)
+
+    sequences = tokenizer.texts_to_sequences(texts)
+    padded = pad_sequences(sequences, maxlen=max_length)
+
+    X_train = padded
+    Y_train = labels_one_hot
+
+    test_df = pd.read_csv("../Data/test.csv", header=None, names=["label", "title", "review"], nrows=nrows_val)
+    test_texts = test_df["review"].values
+    test_labels = test_df["label"].values
+    test_labels = test_labels - 1
+    test_labels_one_hot = to_categorical(test_labels, num_classes=num_classes)
+    test_sequences = tokenizer.texts_to_sequences(test_texts)
+    
+    X_val = pad_sequences(test_sequences, maxlen=max_length)
+    Y_val = test_labels_one_hot
+    
+    return X_val, X_train, Y_val, Y_train
---
+++ Training_script/plots/plots.py
@@ -0,0 +1,52 @@
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import tkinter as tk
+
+canvas1 = None
+canvas2 = None
+
+def create_plot_window(history, root):
+    global canvas1, canvas2
+    EPOCHS = list(range(1, (len(history.history['loss'])) + 1))
+    train_loss = history.history['loss']
+    val_loss = history.history['val_loss']
+    train_acc = history.history['accuracy']
+    val_acc = history.history['val_accuracy']
+    # win = tk.Toplevel(root)
+    # win.title("CNN Training Metrics")
+    root.geometry("900x700")
+
+    # Wykres funkcji Loss czyli błędu uczenia
+    if canvas1 is not None:
+        canvas1.get_tk_widget().destroy()
+        canvas1 = None
+    fig1 = plt.Figure(figsize=(4.5, 3), dpi=100)
+    ax1 = fig1.add_subplot(111)
+    ax1.plot(EPOCHS, train_loss, marker='o', label='Train Loss')
+    ax1.plot(EPOCHS, val_loss, marker='s', label='Validation Loss')
+    ax1.set_title("Loss")
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Loss")
+    ax1.legend()
+    ax1.grid(True)
+    canvas1 = FigureCanvasTkAgg(fig1, master=root)
+    canvas1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+    canvas1.draw()
+
+    # Wykres Accuracy dokładnosci uczenia - w uproszczeniu  procent poprawnie sklasyfikowanych przykładów (zarówno pozytywnych, 
+    # jak i negatywnych)
+    if canvas2 is not None:
+        canvas2.get_tk_widget().destroy()
+        canvas2 = None
+    fig2 = plt.Figure(figsize=(4.5, 3), dpi=100)
+    ax2 = fig2.add_subplot(111)
+    ax2.plot(EPOCHS, train_acc, marker='o', label='Train Accuracy')
+    ax2.plot(EPOCHS, val_acc, marker='s', label='Validation Accuracy')
+    ax2.set_title("Accuracy")
+    ax2.set_xlabel("Epoch")
+    ax2.set_ylabel("Accuracy")
+    ax2.legend()
+    ax2.grid(True)
+    canvas2 = FigureCanvasTkAgg(fig2, master=root)
+    canvas2.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+    canvas2.draw()
