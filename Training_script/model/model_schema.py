5e20d15bba94a93bf369048313596ea9250c1e31
Simon <smonek33@gmail.com>
2025-06-22 16:09:38 +0200

    made it more readable

---
+++ Training_script/model/model_schema.py
@@ -0,0 +1,31 @@
+
+from keras.layers import Bidirectional
+from tensorflow.keras import regularizers
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from data.data_processing import load_and_prepare_data
+from keras.layers import BatchNormalization
+
+def get_ready(words, max_length):
+    nrows_val = 1000
+    X_val, X_train, Y_val, Y_train = load_and_prepare_data(nrows_val, words, max_length)
+    print( load_and_prepare_data(nrows_val, words, max_length))
+
+    #hiper-zmienne
+    model = Sequential([
+        Embedding(input_dim=words, output_dim=32),
+        Dropout(0.5),
+        Bidirectional(LSTM(32, return_sequences=False)),
+        Dense(16, activation='relu', kernel_regularizer=regularizers.l2(0.01)),
+        BatchNormalization(),
+        Dense(2, activation='softmax')
+    ])
+
+    model.build(input_shape=(None, X_train.shape[1]))
+
+    model.summary()
+
+    #opt adam adadelta adagrad rmsprop
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
+    return X_val, X_train, Y_val, Y_train, model
--- Training_script/sentiment_model_trainer.py
+++ Training_script/sentiment_model_trainer.py
@@ -1,170 +1,21 @@
 import os
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 
-import pandas as pd
-import numpy as np
-import pickle
-from keras.layers import Bidirectional
-from tensorflow.keras import regularizers
-from tensorflow.keras.preprocessing.text import Tokenizer
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-# from sklearn.model_selection import train_test_split
-from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
 import tkinter as tk
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from nltk.corpus import stopwords
-from keras.layers import BatchNormalization
 from tensorflow.keras.callbacks import Callback
 import threading
+from model.model_schema import get_ready
+from plots.plots import create_plot_window
 
-
-# #nltk.download('stopwords')
-stop_words = set(stopwords.words('english'))
-
-def remove_stopwords(text):
-    tokens = text.split()
-    filtered = [w for w in tokens if w.lower() not in stop_words]
-    return " ".join(filtered)
-
-words = None
-max_length = None
-epochs = None
-canvas1 = None
-canvas2 = None
 model = None
 X_train = None
 Y_train = None
 X_val = None
 Y_val = None
-
-def get_ready():
-    global model, X_train, Y_train, X_val, Y_val
-    df = pd.read_csv('../Data/train.csv', header=None, names=['label','title','review'], nrows=1000)
-
-    df['review_clean'] = df['review'].astype(str).apply(remove_stopwords)
-
-    texts = df['review_clean'].values
-    labels = df['label'].values
-
-    labels = labels - 1
-
-    num_classes = len(np.unique(labels))
-
-    labels_one_hot = to_categorical(labels, num_classes=num_classes)
-
-    tokenizer = Tokenizer(num_words=words, oov_token="<DOV>")
-
-    tokenizer.fit_on_texts(texts)
-
-    with open('../Trained_models/Dictionaries/tokenizer.pkl', 'wb') as f:
-        pickle.dump(tokenizer, f)
-
-    sequences = tokenizer.texts_to_sequences(texts)
-
-    padded = pad_sequences(sequences, maxlen=max_length)
-
-    # X_train, X_val, Y_train, Y_val = train_test_split(padded, labels_one_hot, test_size=0.2, random_state=42)
-
-    X_train = padded
-    Y_train = labels_one_hot
-
-    test_df = pd.read_csv("../Data/test.csv", header=None, names=["label", "title", "review"], nrows=10000)
-    test_texts = test_df["review"].values
-    test_labels = test_df["label"].values
-    test_labels = test_labels - 1
-    test_labels_one_hot = to_categorical(test_labels, num_classes=num_classes)
-
-    test_sequences = tokenizer.texts_to_sequences(test_texts)
-    X_val = pad_sequences(test_sequences, maxlen=max_length)
-    Y_val = test_labels_one_hot
-
-    # early_stopping = EarlyStopping(
-    #     monitor='val_loss',
-    #     patience=5,
-    #     restore_best_weights=True
-    # )
-
-    #hiper-zmienne
-    model = Sequential([
-        Embedding(input_dim=words, output_dim=32),
-        Dropout(0.5),
-        Bidirectional(LSTM(32, return_sequences=False)),
-        Dense(16, activation='relu', kernel_regularizer=regularizers.l2(0.01)),
-        BatchNormalization(),
-        Dense(2, activation='softmax')
-    ])
-
-    model.build(input_shape=(None, padded.shape[1]))
-
-    model.summary()
-
-    #opt adam adadelta adagrad rmsprop
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-
-def create_plot_window():
-    global canvas1, canvas2
-    EPOCHS = list(range(1, (len(history.history['loss'])) + 1))
-    train_loss = history.history['loss']
-    val_loss   = history.history['val_loss']
-    train_acc  = history.history['accuracy']
-    val_acc    = history.history['val_accuracy'] # symuluje overfitting
-    # win = tk.Toplevel(root)
-    # win.title("CNN Training Metrics")
-    root.geometry("900x700")
-
-    # Wykres funkcji Loss czyli błędu uczenia
-    if canvas1 is not None:
-        canvas1.get_tk_widget().destroy()
-        canvas1 = None
-        print("deleted 1")
-    fig1 = plt.Figure(figsize=(4.5, 3), dpi=100)
-    ax1 = fig1.add_subplot(111)
-    ax1.plot(EPOCHS, train_loss, marker='o', label='Train Loss')
-    ax1.plot(EPOCHS, val_loss, marker='s', label='Validation Loss')
-    ax1.set_title("Loss")
-    ax1.set_xlabel("Epoch")
-    ax1.set_ylabel("Loss")
-    ax1.legend()
-    ax1.grid(True)
-    canvas1 = FigureCanvasTkAgg(fig1, master=root)
-    canvas1.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-    canvas1.draw()
-
-    # Wykres Accuracy dokładnosci uczenia - w uproszczeniu  procent poprawnie sklasyfikowanych przykładów (zarówno pozytywnych, 
-    # jak i negatywnych)
-    if canvas2 is not None:
-        canvas2.get_tk_widget().destroy()
-        canvas2 = None
-        print("deleted 2")
-    fig2 = plt.Figure(figsize=(4.5, 3), dpi=100)
-    ax2 = fig2.add_subplot(111)
-    ax2.plot(EPOCHS, train_acc, marker='o', label='Train Accuracy')
-    ax2.plot(EPOCHS, val_acc, marker='s', label='Validation Accuracy')
-    ax2.set_title("Accuracy")
-    ax2.set_xlabel("Epoch")
-    ax2.set_ylabel("Accuracy")
-    ax2.legend()
-    ax2.grid(True)
-    canvas2 = FigureCanvasTkAgg(fig2, master=root)
-    canvas2.get_tk_widget().pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
-    canvas2.draw()
-
-def training_completed():
-    model.save("../Trained_models/Models/sentiment_model_dropout_twice.keras")
-    graph_btn.config(state="active")
-    start_btn.config(state="active")
-    
-def reset_labels():
-    label_epoch.config(text="Epoch: -", fg="black")
-    label_loss.config(text="Loss Before: - vs After: -", fg="black")
-    label_loss_difference.config(text="Difference: -", fg="black")
-    label_val_loss.config(text="Current Validation Loss: -", fg="black")
-    label_val_acc.config(text="Current Validation Accuracy: -", fg="black")
+words = None
+max_length = None
+epochs = None
 
 root = tk.Tk()
 root.geometry("900x400")
@@ -187,8 +38,10 @@ class EpochLogger(Callback):
         self.epoch_count = epoch + 1
         self.root.after(0, lambda: self.label_epoch.config(text=f"Working on Epoch {epoch + 1}..."))
         
+        
     def on_train_end(self, logs=None):
         self.root.after(0, lambda: self.label_epoch.config(text=f"Finished all {self.epoch_count} Epochs."))
+        
         
     def on_epoch_end(self, epoch, logs=None):
         current_loss = logs.get("val_loss")
@@ -216,30 +69,51 @@ class EpochLogger(Callback):
         self.root.after(0, lambda: self.label_val_loss.config(text=f"Current Validation Loss: {val_loss:.4f}"))
         self.root.after(0, lambda: self.label_val_acc.config(text=f"Current Validation Accuracy: {val_accuracy:.4f}"))
       
+
 def assign_variables():
-    global words, max_length, epochs
+    global words, max_length, epochs, X_val, Y_val
     epochs = int(input_epoch.get())
     max_length = int(input_review_length.get())
     words = int(input_dictionary.get())
         
+        
+def reset_labels():
+    label_epoch.config(text="Epoch: -", fg="black")
+    label_loss.config(text="Loss Before: - vs After: -", fg="black")
+    label_loss_difference.config(text="Difference: -", fg="black")
+    label_val_loss.config(text="Current Validation Loss: -", fg="black")
+    label_val_acc.config(text="Current Validation Accuracy: -", fg="black")
+    
+
+def training_completed():
+    model.save("../Trained_models/Models/sentiment_model_dropout_twice.keras")
+    graph_btn.config(state="active")
+    start_btn.config(state="active")
+    
+    
 def start_training():
-    global training_thread
-    graph_btn.config(state="disabled")
+    global training_thread, X_val, X_train, Y_val, Y_train, model
+    early_stopping = EarlyStopping(
+        monitor='val_loss',
+        patience=5,
+        restore_best_weights=True
+    )
     start_btn.config(state="disabled")
     
     assign_variables()
-    get_ready()
+    X_val, X_train, Y_val, Y_train, model = get_ready(words, max_length)
     reset_labels()
     
     def train():
-        global history
+        global history, model
         history = model.fit(
             X_train, Y_train,
             epochs=epochs,
             validation_data=(X_val, Y_val),
             batch_size=32,
-            callbacks=[epoch_callback]
+            callbacks=[epoch_callback, early_stopping]
         )
+    
     
     def monitor_completion():
         def is_completed():
@@ -249,27 +123,18 @@ def start_training():
                 training_completed()
         
         is_completed()
+          
             
     training_thread = threading.Thread(target=train, daemon=True)
     training_thread.start()
     monitor_completion()
-
-
-
-# '''
-# Program tworzenia wykresów do kontroli uczenia SSN/DNN. Wersja anglojęzyczna z bardzo uproszczonym interfejsem GUI.
-# Na użytek Studentów mojej grupy seminaryjnej.
-# (c) 2025 Krzysztof Michalik, Uniwersytet WSB Merito Chorzów/Katowice
-# '''
-
-#     # Symulowane dane dla 5 epok, można je łatwo zastapić realnymi danymi
 
 metrics = tk.Frame(root)
 metrics.pack(pady=20, padx=40)
 
 title_font = ("Helvetica", 12, "bold")
 
-# Header titles for each column
+# Header titles for each column (think big vertical HTML divs)
 tk.Label(metrics, text="Network Variables", font=title_font).grid(row=0, column=0, padx=30, sticky="w")
 tk.Label(metrics, text="Training Metrics", font=title_font).grid(row=0, column=1, padx=30, sticky="w")
 tk.Label(metrics, text="Validation Metrics", font=title_font).grid(row=0, column=2, padx=30, sticky="w")
@@ -281,7 +146,7 @@ label_epoch.grid(row=1, column=0, sticky="w", padx=30, pady=5)
 
 input_epoch = tk.Entry(metrics, fg="black")
 input_epoch.grid(row=2, column=0, sticky="w", padx=30, pady=5)
-input_epoch.insert(0, "2")
+input_epoch.insert(0, "20")
 
 label_review_length = tk.Label(metrics, text="Max length of reviews", fg="black")
 label_review_length.grid(row=3, column=0, sticky="w", padx=30, pady=5)
@@ -295,7 +160,7 @@ label_dictionary.grid(row=5, column=0, sticky="w", padx=30, pady=5)
 
 input_dictionary = tk.Entry(metrics, fg="black")
 input_dictionary.grid(row=6, column=0, sticky="w", padx=30, pady=5)
-input_dictionary.insert(0, "1000")
+input_dictionary.insert(0, "100000")
 
 start_btn = tk.Button(metrics, text="Start Training", bg="lightblue", command=start_training)
 start_btn.grid(row=7, column=0, sticky="w", padx=30, pady=5)
@@ -317,8 +182,9 @@ label_val_loss.grid(row=2, column=2, sticky="w", padx=30, pady=5)
 label_val_acc = tk.Label(metrics, text="Current Validation Accuracy: -", fg="black")
 label_val_acc.grid(row=4, column=2, sticky="w", padx=30, pady=5)
 
-graph_btn = tk.Button(metrics, text="Show graphs", bg="lightblue", command=create_plot_window)
+graph_btn = tk.Button(metrics, text="Show graphs", bg="lightblue", command=lambda: create_plot_window(history, root))
 graph_btn.grid(row=7, column=2, sticky="w", padx=30, pady=5)
+graph_btn.config(state="disabled")
 
 
 epoch_callback = EpochLogger( root, label_epoch, label_loss, label_val_loss, label_loss_difference, label_val_acc)
